c3d901ac43afa372775780a2af74980fddfa20a8
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2025-01-27 10:01:33 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("****Welcome to my to-do app****")
+user_option = input("Choose an option below: \n 1. Add \n 2. View \n 3. Edit \n 4. Delete \n ")
+
+todos =[]
+if user_option == "1":
+    # add tasks
+    todo = input("Enter to-do:\n")
+    todos.append(todo)
+    print(f"{todo} has been added successfully")
+elif user_option == "2":
+    pass
+elif user_option == "3":
+    pass
+elif user_option == "4":
+    pass
+else:
+    pass
+
